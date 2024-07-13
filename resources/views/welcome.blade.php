8263dafec564fd17f184862bf16b8c952050fd7c
Abinesh <abineshsj96@gmail.com>
2024-07-13 17:34:54 +0530

    bootstrap implementation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,46 +1,110 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-	<title>Mariyamman Indian Bank</title>
-	<link rel="stylesheet" href="{{ asset('app.css') }}">
-	<link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mariyamman Indian Bank</title>
+    <link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap.min.css') }}">
+    <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
+    <style>
+        body {
+            background-color: #f0f2f5;
+            font-family: 'Roboto', sans-serif;
+            margin: 0;
+            padding: 0;
+            overflow-x: hidden;
+        }
+        header {
+        /*background-color: #d6d6d6;*/
+            background-color: #f0f2f5;
+        }
+        .logo-container img {
+            height: 150px;
+            width: 150px;
+        }
+        .logo-container span {
+            font-size: 1.5rem;
+        }
+        .card {
+            height: 200px;
+            background-color: #f2f2f2;
+        }
+        div.card {
+          box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+          text-align: left;
+        }
+    </style>
 </head>
-<body>
-	<header class="header">
-        <img src="no-background-logo.png" height="150" width="150">
-        <span><strong>Welcome to Mariyamman Indian Bank</strong></span>
+<body class="position-sticky">
+    <header class="row py-1">
+        <div class="col-12 col-md-6 d-flex align-items-center logo-container">
+            <img src="{{ asset('no-background-logo.png') }}" alt="Bank Logo">
+            <span>Mariyamman Indian Bank</span>
+        </div>
+        <div class="col-12 col-md-6 d-flex justify-content-center justify-content-md-flex-end align-items-center">
+            <ul class="list-inline mb-0">
+                <li class="list-inline-item"><a class="text-decoration-none text-dark h6" href="#">Netbanking</a></li>
+                <li class="list-inline-item"><a class="text-decoration-none text-dark h6" href="#">Contact Us</a></li>
+            </ul>
+        </div>
     </header>
-    <div class="title"><marquee behavior="scroll" direction="left" scrollamount="8"><strong>QUICK LINKS</strong></marquee></div>
-    <div class="nav-container">
-        <div class="nav-column">
-            <ul>
-                <li><a href="{{ url('register') }}">Open savings account</a></li>
-                <li><a href="{{ url('login') }}">Netbanking login</a></li>
-            </ul>
-        </div>
-        <div class="nav-column">
-            <ul>
-                <li><a href="{{ url('whoami') }}">Forgot account number</a></li>
-                <li><a href="{{ url('findme') }}">Forgot password</a></li>
-            </ul>
-        </div>
-        <div class="nav-column">
-            <ul>
-                <li><a href="{{ url('statement') }}">Statement</a></li>
-                <li><a href="{{ url('deposit') }}">Deposit</a></li>
-                <li><a href="{{ url('withdraw') }}">Withdraw</a></li>
-                <li><a href="{{ url('check_balance') }}">Check balance</a></li>
-                <li><a href="{{ url('update_account') }}">Update details</a></li>
-                <li><a href="{{ url('close_account') }}">Account closure</a></li>
-            </ul>
-        </div>
-        <div class="nav-column">
-            <ul>
-                <li><a href="{{ url('aboutus') }}">About Us</a></li>
-                <li><a href="{{ url('contactus') }}">Contact Us</a></li>
-            </ul>
+    <nav class="bg-dark text-white">
+        <marquee behavior="scroll" direction="left" scrollamount="8" class="mt-3"><strong>QUICK LINKS</strong></marquee>
+    </nav>
+    <script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <div class="container-fluid mt-1">
+        <div class="row">
+            <div class="col-12 col-md-6 p-3">
+                <div class="card p-3">
+                    <div class="card-body">
+                        <ul class="list-inline">
+                            <li><a class="text-decoration-none m-3" href="{{ url('register') }}">Open savings account</a></li>
+                            <li><a class="text-decoration-none m-3" href="{{ url('login') }}">Netbanking login</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 p-3">
+                <div class="card p-3">
+                    <div class="card-body">
+                        <ul class="list-inline">
+                            <li><a class="text-decoration-none m-3" href="{{ url('whoami') }}">Forgot account number</a></li>
+                            <li><a class="text-decoration-none m-3" href="{{ url('findme') }}">Forgot password</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 p-3">
+                <div class="card p-3">
+                    <div class="card-body">
+                        <ul class="list-inline">
+                            <li><a class="text-decoration-none m-3" href="{{ url('statement') }}">Statement</a></li>
+                            <li><a class="text-decoration-none m-3" href="{{ url('deposit') }}">Deposit</a></li>
+                            <li><a class="text-decoration-none m-3" href="{{ url('withdraw') }}">Withdraw</a></li>
+                            <li><a class="text-decoration-none m-3" href="{{ url('check_balance') }}">Check balance</a></li>
+                            <li><a class="text-decoration-none m-3" href="{{ url('update_account') }}">Update details</a></li>
+                            <li><a class="text-decoration-none m-3" href="{{ url('close_account') }}">Account closure</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 p-3">
+                <div class="card p-3">
+                    <div class="card-body">
+                        <ul class="list-inline p-1">
+                            <li><a class="text-decoration-none m-3" href="{{ url('aboutus') }}">About Us</a></li>
+                            <li><a class="text-decoration-none m-3" href="{{ url('contactus') }}">Contact Us</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
+    <footer class="p-5 d-flex justify-content-center">
+        <div>
+            <span> © 2024 All rights reserved only to <b>Abinesh S.</b></span>
+        </div>
+    </footer>
 </body>
 </html>
 
