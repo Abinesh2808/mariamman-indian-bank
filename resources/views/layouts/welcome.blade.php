30b7d1f8359205bf66d20643cb322348bf38ff69
Abinesh <abineshsj96@gmail.com>
2024-07-13 21:59:47 +0530

    convert header case

--- resources/views/layouts/welcome.blade.php
+++ resources/views/layouts/welcome.blade.php
@@ -39,7 +39,7 @@
     <header class="row py-1">
         <a class="col-12 col-md-6 d-flex align-items-center logo-container text-decoration-none text-dark" href="/">
             <img src="{{ asset('no-background-logo.png') }}" alt="Bank Logo">
-            <span>Mariamman Indian Bank</span>
+            <span>MARIAMMAN INDIAN BANK</span>
         </a>
         <div class="col-12 col-md-6 d-flex justify-content-center justify-content-md-flex-end align-items-center">
             <ul class="list-inline mb-0">
